170985bed3903167ad7e6578ea7ba9b3c97d85eb
Josh Cummings <joshcummingsdesign@gmail.com>
2024-03-08 16:10:23 -0800

    fix test case

--- model-6.py
+++ model-6.py
@@ -1,8 +1,8 @@
-user_input = request.GET.get('order_by')
+order = request.GET.get('order_by')
 allowed_order_fields = ['username', 'email']
 
-if user_input in allowed_order_fields:
-    users = User.objects.order_by(user_input)
+if order in allowed_order_fields:
+    users = User.objects.order_by(order)
 else:
     users = User.objects.all()
 
