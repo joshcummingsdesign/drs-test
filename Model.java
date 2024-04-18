5b9d92657f8b1ad9ca9a6a628efbb32c379accaf
Josh Cummings <joshcummingsdesign@gmail.com>
2024-04-18 16:19:34 -0700

    maven

--- Model.java
+++ Model.java
@@ -1,36 +1,9 @@
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public class UserDAO {
-    private static SessionFactory factory;
-
-    static {
-        try {
-            factory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            System.err.println("Failed to create sessionFactory object." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    public Integer addUser(String username, String password) {
-        Session session = factory.openSession();
-        Transaction tx = null;
-        Integer userId = null;
-        try {
-            tx = session.beginTransaction();
-            User user = new User();
-            user.setUsername(username);
-            user.setPassword(password);
-            userId = (Integer) session.save(user);
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-        return userId;
-    }
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT * FROM users WHERE username = ?1", nativeQuery = true)
+    User findByUsername(String username);
+}
