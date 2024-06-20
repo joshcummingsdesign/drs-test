0dd3f5a444d9913a7955afb8c4912c0f3a3693d9
Josh Cummings <joshcummingsdesign@gmail.com>
2024-06-20 12:24:16 -0700

    nat-lang

---
+++ utils.py
@@ -0,0 +1,6 @@
+    @staticmethod
+    def encrypt_sensitive_value(user, value):
+        aes = AES.new('12345', AES.MODE_CBC, Encryption.get_iv(user))
+        return aes.encrypt(Encryption.pad(value))
+
+    @staticmethod
