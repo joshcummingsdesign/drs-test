2479c62cefefb7ba7f3f4461477571e2707a56fe
Josh Cummings <joshcummingsdesign@gmail.com>
2024-10-03 16:27:17 -0700

    test

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 from django.db import connection
 
-# A comment
 def get_users(username):
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
