9cb6a7cc139f75928377cfbc402caad78fc28e44
Josh Cummings <joshcummingsdesign@gmail.com>
2025-04-23 09:24:18 -0700

    test

--- auth.py
+++
@@ -1,3 +0,0 @@
-access_key_id = "ASIAFAKEKEY123456789"
-secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYFAKESECRETKEY"
-session_token = "FQoGZXIvYXdzEGMaDPfakeEXAMPLETOKENbFjq4y1y3s7D/stage/123456EXAMPLETOKENmorebase64data=="
